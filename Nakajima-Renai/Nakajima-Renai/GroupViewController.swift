6aac964aab3514536fb6995651dac69c1319313a
b1013120 <b1013120@fun.ac.jp>
2016-06-11 20:39:11 +0900

    現在考えられるswiftファイルを作成

---
+++ Nakajima-Renai/Nakajima-Renai/GroupViewController.swift
@@ -0,0 +1,9 @@
+//
+//  GroupViewController.swift
+//  Nakajima-Renai
+//
+//  Created by 佐藤孝大 on 2016/06/11.
+//  Copyright © 2016年 Let'sGoShinGo. All rights reserved.
+//
+
+import Foundation
---
+++ Nakajima-Renai/Nakajima-Renai/LoginViewController.swift
@@ -0,0 +1,9 @@
+//
+//  LoginViewController.swift
+//  Nakajima-Renai
+//
+//  Created by 佐藤孝大 on 2016/06/11.
+//  Copyright © 2016年 Let'sGoShinGo. All rights reserved.
+//
+
+import Foundation
