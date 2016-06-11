d7f9a5aaa41307be6e82d3ee68d4a06a0016a00a
codepro <codepro@enPiT2012MBA-06.local>
2016-06-11 16:43:36 +0900

    セルが遷移できるようになった

---
+++ Nakajima-Renai/Nakajima-Renai/MissionDetailViewController.swift
@@ -0,0 +1,27 @@
+//
+//  MissionDetailViewController.swift
+//  Nakajima-Renai
+//
+//  Created by codepro on 2016/06/11.
+//  Copyright © 2016年 Let'sGoShinGo. All rights reserved.
+//
+
+import UIKit
+
+class MissionDetailViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+    
+}
---
+++ Nakajima-Renai/Nakajima-Renai/MissionListViewController.swift
@@ -0,0 +1,54 @@
+//
+//  MissionListViewController.swift
+//  Nakajima-Renai
+//
+//  Created by codepro on 2016/06/11.
+//  Copyright © 2016年 Let'sGoShinGo. All rights reserved.
+//
+
+import UIKit
+
+class MissionListViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.delegate = self
+        tableView.dataSource = self
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    // セルに表示するテキスト
+    let texts = ["ミッション1", "ミッション2", "ミッション3"]
+    
+    
+    // セルの行数
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return texts.count
+    }
+    
+    
+    //セルの内容を変更
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell: UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Cell")
+        
+        cell.textLabel?.text = texts[indexPath.row]
+        return cell
+    }
+    
+    //セルをタップ
+    func tableView(table: UITableView, didSelectRowAtIndexPath indexPath:NSIndexPath) {
+        let storyboard = UIStoryboard(name: "MissionDetailViewController", bundle: NSBundle.mainBundle())
+        let appInfoViewController: MissionDetailViewController = storyboard.instantiateViewControllerWithIdentifier("missiondetail") as! MissionDetailViewController
+        self.navigationController?.pushViewController(appInfoViewController, animated: true)
+        
+    }
+}
